74304868680166199ea38b6429806bc936d8fffb
been <44362272+dc1218@users.noreply.github.com>
2022-07-26 14:08:36 +0000

    test cmake

---
+++ src/chat/main.cpp
@@ -0,0 +1,7 @@
+#include "log.hpp"
+
+int main(int argc,char** argv)
+{
+    log("this is chat");
+    return 0;
+}
---
+++ src/common/log.hpp
@@ -0,0 +1,10 @@
+#ifndef __LOG_HPP__
+#define __LOG_HPP__
+#include <iostream>
+
+void log(const char* msg)
+{
+    std::cout << msg << std::endl;
+}
+
+#endif
